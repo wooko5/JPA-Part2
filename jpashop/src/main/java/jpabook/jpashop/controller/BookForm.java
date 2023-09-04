38d27f239cdc31be758e130c963d63970848f4cf
Jaeuk Oh <wooko5@naver.com>
2023-09-04 17:53:19 +0900

    Update BookForm.java

--- jpashop/src/main/java/jpabook/jpashop/controller/BookForm.java
+++ jpashop/src/main/java/jpabook/jpashop/controller/BookForm.java
@@ -4,17 +4,18 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.NotEmpty;
 
 @Getter
 @Setter
 public class BookForm {
+    //TODO: 검증(validation 넣어보기)
     private Long id;
     @NotBlank(message = "책의 제목은 공백일 수 없습니다.")
     private String name;
-    @NotNull(message = "책의 가격은 공백일 수 없습니다.")
+    @NotBlank(message = "책의 가격은 공백일 수 없습니다.")
     private int price;
-    @NotNull(message = "책의 재고는 공백일 수 없습니다.")
+    @NotBlank(message = "책의 재고는 공백일 수 없습니다.")
     private int stockQuantity;
 
     @NotBlank(message = "작가는 공백일 수 없습니다.")
