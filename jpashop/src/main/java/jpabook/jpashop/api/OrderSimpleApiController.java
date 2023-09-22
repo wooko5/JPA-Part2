787fef3906dcb171f4c978e250faaa5a68aeb42b
Jaeuk Oh <wooko5@naver.com>
2023-09-22 11:16:09 +0900

    OrderSimpleApiController GetMapping 경로변경

--- jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -21,10 +21,10 @@ public class OrderSimpleApiController {
 
     private final OrderRepository orderRepository;
 
-    @GetMapping("/api/vi/simple-orders")
-    public List<Order> ordersV1(){
+    @GetMapping("/api/v1/simple-orders")
+    public List<Order> ordersV1() {
         List<Order> all = orderRepository.findAllByString(new OrderSearch());
-        for(Order order : all){
+        for (Order order : all) {
             order.getMember().getName(); // Lazy 강제 초기화
             order.getDelivery().getAddress(); // Lazy 강제 초기화
         }
